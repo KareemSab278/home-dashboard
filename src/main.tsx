1cfd898b92e1169a717698afa00836f8d95303d5
kareemsab278 <kareemsab278@gmail.com>
2026-09-03 20:54:28 +0100

    go fullscreen on start always

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Router } from "./Router";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
     <React.StrictMode>
         <Router />
     </React.StrictMode>,
 );
+
+getCurrentWindow().setFullscreen(true).catch((err) => {
+    console.error("Failed to set fullscreen:", err);
+});
